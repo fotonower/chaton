ee47f52da3153c433e7f8f84fa07e8d68c04c388
Hidtosh <mhand_rahani@yahoo.fr>
2018-05-15 10:30:48 +0200

    https://github.com/fotonower/raspberrypi/issues/1 script camera pi

---
+++ test_camera.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+"""
+
+
+def cameraGetPic():
+    print("cameraGetPic")
+
+def cameraRecordVideo():
+    print("cameraRecordVideo")
+
+
+while True:
+    try:
+        print("\n1 - Start get picture ")
+        print("2 - Start record video ")
+        print("3 - Exit \n")
+        user_input = int(raw_input("Please enter your choice (1) - (2) - (3) : "))
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+        continue
+    else:
+        if(user_input > 3):
+            continue
+        else:
+            #we're ready to exit the loop.
+            break
+
+if user_input == 1: 
+    cameraGetPic()
+if user_input == 2:
+    cameraRecordVideo()
+if user_input == 3:
+    print("Good bye ! ")
+    exit()
