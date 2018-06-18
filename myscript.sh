163c5f1237a09f36821c3f3219b736b2ef496371
martigan <martin@fotonower.com>
2018-06-18 11:32:18 +0200

    script with path git add -u ./git add -u ./

--- myscript.sh
+++ myscript.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 for pid in $(pidof -x myscript.sh); do
     if [ $pid != $$ ]; then
+	echo 'script allready launched'
         exit 1
     fi
 done
-python get_pic_by_seconds.py
+echo "launching script"
+python /home/pi/workarea/git/raspberrypi/get_pic_by_seconds.py
+echo "after launch"
