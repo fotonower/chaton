aa7e2b4b34b720a766887b203bf37917f9891f5f
moilerat <inscription@moilerat.com>
2018-06-22 22:48:55 +0000

    wip prepare script for race

--- get_pic_by_seconds.py
+++ get_pic_by_seconds.py
@@ -1,19 +1,27 @@
 from picamera import PiCamera
 from datetime import datetime
 camera = PiCamera()
-import os
+import os, sys
 from time import sleep
 camera.start_preview()
 camera.rotation = 180
 folder = ""
 last_hour = ""
+
 while True:
+    sys.stdout.write(".")
+    sys.stdout.flush()
     now = datetime.now()
+#    now_date = str(now).replace(":", "_").replace(" ", "").replace("-", "_").replace(".", "_").replace("\/", "_")
+#    print(now_date)
     day = now.strftime("%d%m%Y")
     hour = now.strftime("%H")
     minutes = now.strftime("%M")
     if last_hour != hour:
-        folder = '/home/pi/Desktop/images/%s/%s/%s' % day,hour,minutes
-        os.makedirs(folder)
-    camera.capture(folder + '/image%s.jpg' % now.strftime("%S"))
-    sleep(1)
+        folder = '/home/pi/Desktop/images' + str(day) + "/" + str(hour) + "/" + str(minutes)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+    camera.capture(folder + '/image' + str(now.strftime("%S.%f")) + ".jpg")
+#    camera.capture(os.path.join(folder, "image_" + str(now_date) + ".jpg"))
+    sleep(0.1)
