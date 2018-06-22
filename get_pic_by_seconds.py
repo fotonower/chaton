ff302891b9940155019b19a66307a9bcffa5606a
moilerat <inscription@moilerat.com>
2018-06-22 22:57:09 +0000

    prepare script for first run

--- get_pic_by_seconds.py
+++ get_pic_by_seconds.py
@@ -1,22 +1,27 @@
 from picamera import PiCamera
 from datetime import datetime
 camera = PiCamera()
-import os
+import os, sys
 from time import sleep
 camera.start_preview()
 camera.rotation = 180
 folder = ""
-last_minute = ""
+last_hour = ""
+
 while True:
+    sys.stdout.write(".")
+    sys.stdout.flush()
     now = datetime.now()
+#    now_date = str(now).replace(":", "_").replace(" ", "").replace("-", "_").replace(".", "_").replace("\/", "_")
+#    print(now_date)
     day = now.strftime("%d%m%Y")
     hour = now.strftime("%H")
     minutes = now.strftime("%M")
-    if int(hour) >= 22:
-        exit(0)
-    if last_minute != minutes:
-        folder = '/home/pi/Desktop/images/{}/{}/{}'.format(str(day),str(hour),str(minutes))
-        os.makedirs(folder)
-	last_minute = minutes
-    camera.capture(folder + '/image%s.jpg' % now.strftime("%S"))
-    sleep(1)
+    if last_hour != hour:
+        folder = '/home/pi/Desktop/images' + str(day) + "/" + str(hour) + "/" + str(minutes)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+    camera.capture(folder + '/image' + str(now.strftime("%S.%f")) + ".jpg")
+#    camera.capture(os.path.join(folder, "image_" + str(now_date) + ".jpg"))
+    sleep(0.1)
