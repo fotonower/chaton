5035c362f6931ed4dc47f48d07ba4464845dfc9c
martigan <martin@fotonower.com>
2018-06-27 13:30:08 +0200

    Fotonower/Velours#659 gestion end time dans le script sh en 2eme parametre

--- myscript.sh
+++ myscript.sh
@@ -11,7 +11,11 @@ if [-n $1]
 then
     pause=" -p $1"
 fi
+
+if [-n $2]
+then
 echo "launching script"
+fi
 
 python /home/pi/workarea/git/raspberrypi/get_pic_by_seconds.py$pause
 echo "after launch"
