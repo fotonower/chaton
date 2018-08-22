2958dbefed99e87543bb0a0aac2e2520b8eaed95
martigan <martin@fotonower.com>
2018-08-22 17:30:45 +0200

    Fotonower/Metolour#336 correction makedirs

--- get_pic_by_seconds.py
+++ get_pic_by_seconds.py
@@ -8,8 +8,8 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-f", "--folder", action="store", type="string", dest="folder", default="/home/pi/Desktop/images",
                       help="base folder where photos are saved")
-parser.add_option("-p", "--pause", action="store", type="int", dest="pause",
-                      default=1, help="interval between photos")
+parser.add_option("-p", "--pause", action="store", type="float", dest="pause",
+                      default=1.0, help="interval between photos")
 parser.add_option("-r", "--rotation", action="store", type="int", dest="rotation",
                       default=180, help="rotation of photos")
 parser.add_option("-e", "--end", action="store", type="int", dest="end",
@@ -44,7 +44,8 @@ while True:
         exit(0)
     if last_minute != minutes:
         folder = os.path.join(x.folder,'{}/{}/{}'.format(str(day),str(hour),str(minutes)))
-        os.makedirs(folder)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
 	last_minute = minutes
     camera.capture(folder + '/image_{}_{}_{}_{}.jpg'.format(str(day),str(hour),str(minutes),now.strftime("%S")))
     sleep(x.pause)
