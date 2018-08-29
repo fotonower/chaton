d84f7f4815e5faa5063c14ffb6baab59ffeda04a
Victor Reutenauer <inscription@moilerat.com>
2018-08-30 00:41:44 +0200

    add try catch and fix write str

--- raspberry_camera/python/lib/conn_folder.py
+++ raspberry_camera/python/lib/conn_folder.py
@@ -14,87 +14,98 @@ class FolderReadWrite():
 
     def aux_increment_file(self, counter_increment):
 
-        if not os.path.exists(os.path.dirname(counter_increment)):
-            os.makedirs(os.path.dirname(counter_increment))
+        try :
+            if not os.path.exists(os.path.dirname(counter_increment)):
+                os.makedirs(os.path.dirname(counter_increment))
 
-        counter_init = 0
-        if os.path.exists(counter_increment) :
-            f = open(counter_increment, "r")
+            counter_init = 0
+            if os.path.exists(counter_increment) :
+                f = open(counter_increment, "r")
 
-            counter_init_read = f.readline().rstrip()
+                counter_init_read = f.readline().rstrip()
 
-            if counter_init_read.isdigit():
-                counter_init = int(counter_init_read)
-            else :
-                print ("Error reading : " + str(counter_init_read) + ", expected digits !")
+                if counter_init_read.isdigit():
+                    counter_init = int(counter_init_read)
+                else :
+                    print ("Error reading : " + str(counter_init_read) + ", expected digits !")
 
+                f.close()
+
+                os.remove(counter_increment)
+
+            counter_current = counter_init + 1
+
+            f = open(counter_increment, "w+")
+            f.write(str(counter_current))
             f.close()
-
-            os.remove(counter_increment)
-
-        counter_current = counter_init + 1
-
-        f = open(counter_increment, "w+")
-        f.write(counter_current)
-        f.close()
+        except Exception as e:
+            print(str(e))
 
 
 
     def append_photo(self, photopath, date):
+        try :
+            day = date.strftime("%d%m%Y")
+    #        hour = date.strftime("%H")
+    #        minutes = date.strftime("%M")
 
-        day = date.strftime("%d%m%Y")
-#        hour = date.strftime("%H")
-#        minutes = date.strftime("%M")
+            counter_photo_path = os.path.join(self.root_folder, day, self.counter_photo)
 
-        counter_photo_path = os.path.join(self.root_folder, day, self.counter_photo)
-
-        self.aux_increment_file(counter_photo_path)
-
+            self.aux_increment_file(counter_photo_path)
+        except Exception as e:
+            print (str(e))
 
 
 
     def upload_one(self, photopath, date):
+        try :
+            day = date.strftime("%d%m%Y")
+    #        hour = date.strftime("%H")
+    #        minutes = date.strftime("%M")
 
-        day = date.strftime("%d%m%Y")
-#        hour = date.strftime("%H")
-#        minutes = date.strftime("%M")
+            counter_upload_path = os.path.join(self.root_folder, day, self.counter_upload)
 
-        counter_upload_path = os.path.join(self.root_folder, day, self.counter_upload)
-
-        self.aux_increment_file(counter_upload_path)
-
+            self.aux_increment_file(counter_upload_path)
+        except Exception as e:
+            print (str(e))
 
 
     def create_portfolio(self, portfolio_id, date):
+        try :
+            day = date.strftime("%d%m%Y")
 
-        day = date.strftime("%d%m%Y")
+            path_record_portfolio_id = os.path.join(self.root_folder, day, self.portfolio_id)
 
-        path_record_portfolio_id = os.path.join(self.root_folder, day, self.portfolio_id)
-
-        if os.path.exists(path_record_portfolio_id) :
-            print ("Error with existing portfolio !")
-        else :
-            f = open(path_record_portfolio_id)
+            if os.path.exists(path_record_portfolio_id) :
+                print ("Error with existing portfolio !")
+            else :
+                f = open(path_record_portfolio_id)
+        except Exception as e:
+            print(str(e))
 
 
 
     # VR needs maybe to be changed to be by name !
     def get_today_portfolio_id(self, date):
+        try:
+            day = date.strftime("%d%m%Y")
 
-        day = date.strftime("%d%m%Y")
+            path_record_portfolio_id = os.path.join(self.root_folder, day, self.portfolio_id)
 
-        path_record_portfolio_id = os.path.join(self.root_folder, day, self.portfolio_id)
-
-        if os.path.exists(path_record_portfolio_id) :
-            f = open(path_record_portfolio_id, "r")
-            port_id_str = f.readline().lstrip()
-            if port_id_str.isdigit() :
-                return int(port_id_str)
+            if os.path.exists(path_record_portfolio_id) :
+                f = open(path_record_portfolio_id, "r")
+                port_id_str = f.readline().lstrip()
+                if port_id_str.isdigit() :
+                    return int(port_id_str)
+                else :
+                    print ("Expected digit : " + str(port_id_str))
+                    return 0
             else :
-                print ("Expected digit : " + str(port_id_str))
                 return 0
-        else :
-            return 0
+        except Exception as e:
+            print(str(e))
+
+
 
 if __name__ == "__main__" :
     print("To use for test")
