330b25a574caa8ff9b5ca79bad23ef0002dbda7f
martigan <martin@fotonower.com>
2018-09-12 16:03:14 +0200

    crontab test with hours as variable

--- make_cron.sh
+++ make_cron.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+echo "updating fotonower package"
 sudo pip install --upgrade fotonower
+echo "backuping current crontab"
 crontab -l > crontab.backup.`date "+%Y%m%d%H_%M"`
+echo "creating new crontab file with config.txt"
 cat config.txt crontab_raspberry.txt > crontab.txt
-crontab crontab.txt
+echo "you need now to source crontab with crontab crontab.txt"
+#crontab crontab.txt
