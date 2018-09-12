00f450a114c7f26537a4feec9343fe408dd4ec7f
martigan <martin@fotonower.com>
2018-09-12 14:11:05 +0200

    make cron

--- make_cron.sh
+++ make_cron.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-
+sudo pip install --upgrade fotonower
+crontab -l > crontab.backup.`date "+%Y%m%d%H_%M"`
 cat config.txt crontab_raspberry.txt > crontab.txt
 crontab crontab.txt
