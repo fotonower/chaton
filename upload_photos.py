422dcbecb12d47398ce8d3d9052afdd4074c0130
martigan <martin@fotonower.com>
2018-12-05 11:22:29 +0100

    Fotonower/Velours#785 try catch on numpy operation

--- upload_photos.py
+++ upload_photos.py
@@ -24,14 +24,16 @@ def check_pictures(folder,day,hour,minutes,lsr,threshold, factor):
         if "mimage" in filename:
             continue
         img = np.array(cv2.imread(file))
-        test = np.sum(np.mean(np.mean(img, axis=0), axis=0))
+        try:
+            test = np.sum(np.mean(np.mean(img, axis=0), axis=0))
+        except:
+            print("erreur with numpy sum mean, uploading full image without sum")
+            continue
         filename = str(test) + 'm' + filename
         new_path = os.path.join(folder, str(test) + 'm' + filename)
         if test < threshold:
             print('rewriting img, sum of mean RGB is {}'.format(test))
-
             res = cv2.resize(img, (0, 0), fx=factor, fy=factor, interpolation=cv2.INTER_CUBIC)
-
             new_path = os.path.join(folder,str(test) + 'm' + filename)
             cv2.imwrite(new_path, res)
             new_folder= os.path.join('/'.join(folder.split('/')[:-3]),"deleted/"+ '/'.join(folder.split('/')[-3:]))
