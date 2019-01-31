27c38061ca6db8a27e5aa1df700ce8b2d5f38bab
martigan <martin@fotonower.com>
2019-01-31 12:31:02 +0100

    Fotonower/Metolour#434 test acquisition son

--- test_gpio/get_sound.py
+++ test_gpio/get_sound.py
@@ -1,5 +1,6 @@
 import sounddevice as sd
 import numpy as np
+from scipy.io.wavfile import write
 
 fs = 44100
 sd.default.device = 'USB Audio Device'
@@ -9,3 +10,5 @@ myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=1)
 
 #myrecording = sd.rec(int(duration * fs))
 sd.wait()
+
+write('test.wav', 44100, myrecording)
